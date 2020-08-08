e4ae847da145a9f3be83b4fc68668508b4f14f4d
ikawaha <ikawaha@users.noreply.github.com>
2020-08-08 11:15:38 +0900

    Update

--- internal/data/bindata51.go
+++ internal/data/bindata51.go
@@ -17,7 +17,7 @@ func dictUnidictBz() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "dict/unidict.bz", size: 524288, mode: os.FileMode(420), modTime: time.Unix(1596546051, 0)}
+	info := bindataFileInfo{name: "dict/unidict.bz", size: 524288, mode: os.FileMode(420), modTime: time.Unix(1596852912, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
